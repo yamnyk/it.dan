e5f9d29c8915fb76a96a9e5f61b7bc09b422f9f7
YAMNYK <yamnyk.vlog@gmail.com>
2018-05-01 21:34:36 +0300

    solve get area task

--- SchoolProjects/src/it/dan/homeWork/getShapeArea/service/FindArea.java
+++ SchoolProjects/src/it/dan/homeWork/getShapeArea/service/FindArea.java
@@ -25,7 +25,7 @@ public class FindArea {
 
     private static void printAreaOfRectangle(Rectangle ab) {
         if(ab.area() > 0){
-            System.out.printf("Te area of rectangle width = %d, height = %d is - %d",
+            System.out.printf("Te area of rectangle width = %d, height = %d is: %d",
                     ab.getWidth(), ab.getHeight(), ab.area());
         } else {
             System.out.println("Uncorrected parameter");
@@ -34,7 +34,7 @@ public class FindArea {
 
     private static void printAreaOfCircle(Circle o) {
         if(o.area() > 0){
-            System.out.printf("The area of circle with radius %d is - %d \n", o.getRadius(), o.area());
+            System.out.printf("The area of circle with radius %d is: %d \n", o.getRadius(), o.area());
         } else {
             System.out.println("Uncorrected parameter");
         }
@@ -42,7 +42,7 @@ public class FindArea {
 
     private static void printAreaOfTriangle(Triangle abc) {
         if(abc.area() != -1){
-            System.out.printf("The area of triangle %d, %d, %d is - %d \n",
+            System.out.printf("The area of triangle %d, %d, %d is: %d \n",
                     abc.getASide(), abc.getBSide(), abc.getCSide(), abc.area());
         } else {
             System.out.println("Uncorrected parameter");
