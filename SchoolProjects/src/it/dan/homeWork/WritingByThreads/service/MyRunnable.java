ca6727ae5da56c2b9519c1c6104836588218a6b1
YAMNYK <yamnyk.vlog@gmail.com>
2018-05-15 14:49:18 +0300

    WritingByThreads run() separated by 2 other methods

--- SchoolProjects/src/it/dan/homeWork/WritingByThreads/service/MyRunnable.java
+++ SchoolProjects/src/it/dan/homeWork/WritingByThreads/service/MyRunnable.java
@@ -6,13 +6,14 @@ import java.io.IOException;
 public class MyRunnable implements Runnable {
     @Override
     public void run() {
-        //creating filepath
-        String sep = System.getProperty("file.separator");
-        String appDir = "/home/yamnyk/Downloads"; //path to file directory
-        String filePath = appDir + sep + "myFirstFile.txt"; //full path to the file
+        String pathToFile = getFilePath("/home/yamnyk/Downloads");
+        writeToFile(pathToFile);
+    }
 
+    private void writeToFile(String pathToFile) {
         try {
-            FileWriter fw = new FileWriter(filePath, true);
+            FileWriter fw = new FileWriter(pathToFile, true); //'true' flag means that old data
+                                                                 // will be saved while writing
             fw.write("I'm hot, and you are not! " + "Thread name - "
                     + Thread.currentThread().getName() + "\n");
             fw.flush();
@@ -20,5 +21,11 @@ public class MyRunnable implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getFilePath(String appDir) {
+        // creating filepath
+        String sep = System.getProperty("file.separator"); //gets the system file separator symbol
+        return appDir + sep + "myFirstFile.txt"; //if file doesn't exist, it will be created
     }
 }
